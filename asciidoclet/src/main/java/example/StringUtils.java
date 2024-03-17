ab566aca3fc38b89fd01ebb2bb5c75849e3c7b7e
Abel Salgado Romero <abelromero@gmail.com>
2024-03-17 19:33:40 +0100

    WIP

---
+++ asciidoclet/src/main/java/example/StringUtils.java
@@ -0,0 +1,15 @@
+package example;
+
+public class StringUtils {
+
+    /**
+     * This is a comment.
+     *
+     * @param haystack the haystack
+     * @param needle   can be dangerous
+     * @return true if lucky
+     */
+    public boolean contains(String haystack, String needle) {
+        return haystack.contains(needle);
+    }
+}
